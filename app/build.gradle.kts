0ec241854e7d1d357f67dae3101ff926930a36e3
AlfonsoNavio <alfonso.navio@wipay.es>
2026-01-22 16:29:51 +0100

    Actualizada version de Google Services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.googleDevtoolsKsp)
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 android {
